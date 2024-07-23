b804bb80f320f1b200df149941126aae399a27c3
Jinu Yi <dionisos198@naver.com>
2024-07-24 04:12:00 +0900

    Feat: Kafka 를 통한 메시징 전송

--- Catalog/src/main/java/org/example/repository/CatalogRepository.java
+++ Catalog/src/main/java/org/example/repository/CatalogRepository.java
@@ -1,8 +1,11 @@
 package org.example.repository;
 
+import java.util.Optional;
 import org.example.entity.CatalogEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CatalogRepository extends JpaRepository<CatalogEntity,Long> {
+
+    Optional<CatalogEntity> findByProductId(String productId);
 
 }
